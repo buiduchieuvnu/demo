16a0ded8e33637b4d3eb399010e834b5150fe8cb
dtquyen <dtquyen@vnpt.vn>
2023-03-03 11:16:26 +0700

    crud ok

--- 2-Working/sample-code/demo/src/main/java/com.example.demo/entity/Product.java
+++ 2-Working/sample-code/demo/src/main/java/com.example.demo/entity/Product.java
@@ -1,30 +1,34 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+
+import javax.persistence.*;
 
 @Entity
-@Table(name = "Products")
+@Table(name = "products")
 public class Product {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
-    private String Id;
+    private Integer Id;
 
     @Column
     private String Name;
 
     public Product() {
     }
-    public Product(String id, String name) {
+
+    public Product(Integer id, String name) {
         Id = id;
         Name = name;
     }
 
-    public String getId() {
-        return Id;
+    public void setId(Integer id) {
+        Id = id;
     }
 
-    public void setId(String id) {
-        Id = id;
+    public Integer getId() {
+        return Id;
     }
 
     public String getName() {
