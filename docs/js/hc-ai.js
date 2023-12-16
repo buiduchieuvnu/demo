372d38d10ab0ba8c6e1804764e4fcb6877841db7
Hiếu Bùi Đức <buiduchieuvnu@users.noreply.github.com>
2023-12-16 13:07:53 +0700

    update

--- docs/js/hc-ai.js
+++ docs/js/hc-ai.js
@@ -1,8 +1,11 @@
 (function () {
   "use strict";
 
+  const GG = new GoogleCloudService();
+
   // Trợ lý ảo AI by hieubd
   const btnSendMessage = document.getElementById("btnSendMessage");
+  const btnMicrofone = document.getElementById("btnRecording");
   const sendMessage = document.getElementById("txtMessage");
   
   // const microphone = document.querySelector('.va-microfone');
@@ -11,10 +14,10 @@
      var msg = sendMessage.value;
      //alert(msg);
      addUserChat(msg);
-     questionAI(msg);
      sendMessage.value = "";
+     questionAI(msg);
+     
   });
-
 
   const addUserChat = (msg) => {
     const chatBox = document.getElementById("chatBox");
@@ -55,7 +58,7 @@
     const chatMsg = '<!-- Single Chat Item -->                                                                                                                                                       '
     + '<div class="single-chat-item">                                                                                                                                                  '
     + '<!-- User Avatar -->                                                                                                                                                            '
-    + '<div class="user-avatar mt-1">                                                                                                                                                  '
+    + '<div class="user-avatar mt-1"> <span class="name-first-letter">A</span><img src="img/demo-img/trolyao.png" alt="">                                                                                                                                                 '
     + '</div>                                                                                                                                                                          '
     + '<!-- User Message -->                                                                                                                                                           '
     + '<div class="user-message">                                                                                                                                                      '
@@ -80,7 +83,6 @@
     + '</div>                                                                                                                                                                          '
     + '</div>                                                                                                                                                                          ';
     chatBox.innerHTML += chatMsg;
-    
   }
 
   const getCurrentDateTime = (msg) => {
@@ -102,40 +104,133 @@
 
   }
 
-  const questionAI = (msg) => {
-    var myHeaders = new Headers();
-    myHeaders.append("authority", "qna-openai.netlify.app");
-    myHeaders.append("accept", "*/*");
-    myHeaders.append("accept-language", "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5");
-    myHeaders.append("content-type", "application/json");
-    myHeaders.append("origin", "https://qna-openai.netlify.app");
-    myHeaders.append("referer", "https://qna-openai.netlify.app/?fbclid=IwAR1jUgdl8HYQBcg_aC6nSVBBAGbAHh-8LTOao2_5TJjs8MbnCUD1KHs6sm0");
-    myHeaders.append("sec-ch-ua", "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"");
-    myHeaders.append("sec-ch-ua-mobile", "?0");
-    myHeaders.append("sec-ch-ua-platform", "\"Windows\"");
-    myHeaders.append("sec-fetch-dest", "empty");
-    myHeaders.append("sec-fetch-mode", "cors");
-    myHeaders.append("sec-fetch-site", "same-origin");
-    myHeaders.append("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36");
+  const questionAI = async (msg) => {
+    console.log('AI question: ' + msg);
+    try {
+      const response = await  axios.post('https://qna-openai.netlify.app/api/chat', {
+        input: msg + " nếu câu bạn không đủ kiến thức hoặc không biết thì hãy trả về đúng 1 từ là 'TRA GOOGLE' không trả về cái gì hết ngoài từ đó",
+      });
 
-    var raw = JSON.stringify({
-      "prompt": "AI Y tế",
-      "input": msg
-    });
+      if (!response.data.toString().includes('TRA GOOGLE')) {
+        addBOTChat(response.data);
+        await GG.speech(response.data, 1);
+      } else {
+        const data = JSON.stringify({
+          q: msg,
+          gl: 'vn',
+          hl: 'vi',
+        });
 
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
+        const googleResponse = await axios.post(
+          'https://google.serper.dev/search',
+          data,
+          {
+            headers: {
+              'X-API-KEY': '445e4a16e4972f5654108187b91c6d7f9c73e47e',
+              'Content-Type': 'application/json',
+            },
+          }
+        );
 
-    fetch("https://qna-openai.netlify.app/api/chat", requestOptions)
-      .then(response => response.text())
-      .then(result => addBOTChat(result))
-      .catch(error => console.log('error', error));
+        const answerResponse = await axios.post('https://qna-openai.netlify.app/api/chat', {
+          input: `câu hỏi của tôi là "${msg}" và trả lời là "${googleResponse.data?.organic[0]?.snippet}" tạo thành câu hoàn chỉnh dựa trên câu hỏi và câu trả lời`,
+        });
+
+        addBOTChat(answerResponse.data);
+        await GG.speech(answerResponse.data, 1);
+        //outputText.innerText = 'Trả lời: ' + answerResponse.data;
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log('Xong!');
+    }
 
   }
 
+  // ======================== TRO LY AO ====================================
+   // Tro ly ao
+  //var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  const synth = window.speechSynthesis;
+  recognition.lang = 'vi-VI';
+  recognition.continuous = false;
+
+  const speak = (text) => {
+      if (synth.speaking) {
+          console.error('Busy. Speaking...');
+          return;
+      }
+
+      const utter = new SpeechSynthesisUtterance(text);
+
+      utter.onend = () => {
+          console.log('SpeechSynthesisUtterance.onend');
+      }
+      utter.onerror = (err) => {
+          console.error('SpeechSynthesisUtterance.onerror', err);
+      }
+
+      synth.speak(utter);
+  };
+
+
+  btnMicrofone.addEventListener('click', (e) => {
+    GG.speech("Trợ lý ảo HC mời bạn ra lệnh", 1);
+    e.preventDefault();
+    setTimeout(() => {
+      recognition.start();
+    }, 2000);
+  });
+
+  const handleVoice = (text) => {
+    var handledText = text.toLowerCase();
+    console.log('text: ', handledText);
+    addUserChat(handledText);
+
+    if (handledText.startsWith('hc')) {
+      handledText = handledText.substring(2);
+      questionAI(handledText);
+    } else {
+      if (handledText.includes('mấy giờ')) {
+        const textToSpeech = `Bây giờ là ${moment().hours()} giờ ${moment().minutes()} phút`;
+        GG.speech(textToSpeech, 1);
+        //speak(textToSpeech);
+        return;
+      }
+
+      if (handledText.includes('đăng xuất')) {
+        
+        GG.speech("Trợ lý ảo HC sẽ đăng xuất tài khoản của bạn khỏi phần mềm. Xin chào tạm biệt", 1);
+
+        setTimeout(() => {
+          window.location.href = 'hc-login.html';
+        }, 8000);
+
+        
+        //speak(textToSpeech);
+        return;
+    }
+    }
+
+    
+
+    
+  }
+
+  recognition.onspeechend = () => {
+      recognition.stop();
+  }
+
+  recognition.onerror = (err) => {
+      console.error(err);
+  }
+
+  recognition.onresult = (e) => {
+    console.log('onresult', e);
+    const text = e.results[0][0].transcript;
+    handleVoice(text);
+}
 
 })();
